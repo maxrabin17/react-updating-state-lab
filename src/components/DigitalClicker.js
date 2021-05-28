1498d62539b66f85bf64878589c1f03ed9adea64
maxrabin <max.rabin17@gmail.com>
2021-05-28 14:38:25 -0400

    Completed Assignment

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export class DigitalClicker extends Component {
+    constructor(){
+        super();
+        this.state = {
+            timesClicked: 0,
+        }
+    }
+    
+    handleClick = () => {
+        // let newTimesClicked = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: this.state.timesClicked + 1
+            //timesClicked: newTimesClicked
+        })
+    } 
+
+    render() {
+        return (
+            <button onClick = {this.handleClick} >{this.state.timesClicked}</button>
+        )
+    }
+}
+
+export default DigitalClicker
